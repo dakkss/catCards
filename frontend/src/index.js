e55cb352b20a919f6041b53a3c082cb1924a8261
d4kz <robertdlguerrero670@gmail.com>
2023-09-13 16:33:36 +1000

    Added analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { Analytics } from '@vercel/analytics'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <>
+        <App />
+        <Analytics />
+    </>
+
 );
 
 
