df890f42e4c258873ce852b377ce57b4aeb17db2
d4kz <robertdlguerrero670@gmail.com>
2023-09-13 16:11:03 +1000

    ?

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 const app = express()
-app.use(cors())
+app.use(cors(({origin: true, credentials: true})))
 app.use(express.json())
 
 app.get('/api/v1/catCards', async (req, res) => {
