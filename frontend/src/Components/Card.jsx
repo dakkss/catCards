33266e741e336a1262b9927ebc9595cf9146a2a0
d4kz <robertdlguerrero670@gmail.com>
2023-09-13 16:01:49 +1000

    overflow removed

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
-// import pic from "../Pictures/logo192.png"
+import pic from "../Pictures/logo192.png"
 
-export default function Card({ cat_name, cat_breed }) {
+export default function Card({cat_name, cat_breed}) {
 
     const [catCards, setCatCards] = useState()
 
+    // useEffect(async () => {
+    //     const response = await axios.get("http://localhost:8080/api/v1/catCards")
+    //     setCatCards(response.data)
+    // }, [])
 
     useEffect(() => {
         async function fetchData() {
@@ -13,28 +17,25 @@ export default function Card({ cat_name, cat_breed }) {
             setCatCards(response.data)
         }
         fetchData();
-    }, []);
+      }, []); // Or [] if effect doesn't need props or state
 
     console.log(catCards)
 
-    if (catCards) {
-        return (
-            <div className="flex flex-row flex-wrap justify-center items-center">
-                {catCards.map(card => (
-                    <div key={card.cat_id} className="border-2 mx-5 my-8 w-40 hover:shadow-xl">
-                        <div className="flex justify-center items-center p-5">
-                            <h1>IMAGE HERE</h1>
-                        </div>
-                        <hr />
-                        <div key={card.cat_id} className="px-2 pt-4 pb-3">
-                            <h4 className="pb-5 pt-4">{card.cat_name}</h4>
-                            <h4 className="">{card.cat_breed}</h4>
-                        </div>
+    if (catCards) {return (
+        <div className="flex flex-row flex-wrap justify-center items-center">
+            {catCards.map(card => (
+                <div key={card.cat_id} className="border-2 mx-5 my-8 sm:w-40 md:w-52 hover:shadow-xl">
+                    <div className="flex justify-center items-center p-5">
+                        <h1>IMAGE HERE</h1>
                     </div>
-                ))}
-            </div>
-        )
-    }
+                    <div key={card.cat_id} className="px-2 pt-9 pb-3">
+                        <h4>{card.cat_name}</h4>
+                        <h4>{card.cat_breed}</h4>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )}
 
 
 }
